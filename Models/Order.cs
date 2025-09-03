d590b866c46511e1165a653b0c79a707014085ba
NOUR-AWAWDEH <nourawawdeh95@gmail.com>
2025-09-03 13:47:49 +0300

    Implment the Db

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace OrdersAPI.Models;
 
 public class Order
 {
-    
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Status { get; set; }
+    public Decimal TotalCost { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using OrdersAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("BaseConnection")));
 
 var app = builder.Build();
 
